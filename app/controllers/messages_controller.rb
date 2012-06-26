7913fe4069bc61892e8bb3d6ef7302263ace40ac
nicholashibberd <nicholashibberd@gmail.com>
2012-06-26 14:55:38 +0100

    adding cv

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,8 @@ class MessagesController < ApplicationController
     page = Page.find_by_slug(params[:page_id])       
     message = Message.create(params[:message])
     Notifier.notification(message).deliver
-    redirect_to main_app.host_page_path(page)
+    destination = page.slug == 'contact' ? main_app.host_page_path(page) : '/'
+    redirect_to destination, :notice => 'Thanks for your message!'
   end
     
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def age
+    today = Date.today
+    today.year - 1982 + (today.month - 7 + ((today.day - 12) < 0 ? -1 : 0) < 0 ? -1 : 0)
+  end
 end
